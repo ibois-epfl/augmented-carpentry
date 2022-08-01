60fb92d6ab36498d0e182354baa5b2607ddb83d0
9and3 <andreasettimi39@gmail.com>
2022-08-01 17:51:02 +0200

    CAP: cleaned working version (no opencv)

--- main.cpp
+++ main.cpp
@@ -16,80 +16,60 @@
 
 static void glfwErrorCallback(int error, const char* description)
 {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+    spdlog::error("Glfw Error %d: %s", error, description);
 }
 
 int main(int argc, char** argv)
 {
-    spdlog::info("Welcome to spdlog!");
-
-    // <<<<<<<<<<<< glfw init <<<<<<<<<<<<
-
-    // Create window with graphics context
+    spdlog::set_pattern("%^[%l] %v%$");
+    spdlog::info("Starting glfw window and context...");
+    const char* glsl_version = "#version 130"; // GL 3.0 + GLSL 130
     glfwSetErrorCallback(glfwErrorCallback);
     if (!glfwInit()) exit(EXIT_FAILURE);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     GLFWwindow* window = glfwCreateWindow(1280, 720, "AugmentedCarpentry", nullptr, nullptr);
     if (window == NULL)
     {
-        std::cout << "[ERROR] Failed to create GLFW window" << std::endl;
+        spdlog::error("Failed to create GLFW window");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
 
-    // >>>>>>>>>>>> glfw init >>>>>>>>>>>>
-
-
-    // Setup Dear ImGui context
+    // For more info on imgui structure:https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl3/main.cpp
+    spdlog::info("Starting Dear ImGui...");
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-
-    // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Our state
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-
-
-    // Application main loop
+    spdlog::info("Starting main loop...");
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-
-        // Beginning of frame: update Renderer + Platform backend, start Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // >>>>>>>>>>>> OUR CODE GOES HERE >>>>>>>>>>>>
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
+        // <<<<<<<<<<<< OUR CODE ENDS HERE <<<<<<<<<<<<
 
-
-        // // End of frame: render Dear ImGui
-        // ImGui::Render();
-        // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        // Rendering
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
@@ -102,13 +82,13 @@ int main(int argc, char** argv)
     }
 
     // Cleanup
+    spdlog::info("Cleaning up ImGui. Glfw, Opengl3...");
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
 
     return 0;
 }
