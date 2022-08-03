2baed1d3d0cee763510571255eaf6ee88a47e56f
9and3 <andreasettimi39@gmail.com>
2022-08-03 17:59:39 +0200

    replace cmake config instead target_compile_def

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,38 @@
 
+#include <GL/gl.h>
+#include <GL/glut.h>
+
 #include <iostream>
 #include <string>
-
-#include <GL/glut.h>
-#include <GL/gl.h>
-
-#include "GLFW/glfw3.h"
-#include "GLES2/gl2.h"  // <-- comes with OpenGL3 or needs to be added?
 
 #include "../deps/imgui/imgui.h"
 #include "../deps/imgui/imgui_impl_glfw.h"
 #include "../deps/imgui/imgui_impl_opengl3.h"
-
 #include "../deps/spdlog/include/spdlog/spdlog.h"
+#include "GLES2/gl2.h"  // <-- comes with OpenGL3 or needs to be added?
+#include "GLFW/glfw3.h"
 
-#include "ACConfig.h"
+// #include "ACConfig.h"
 
-#include "AIAC/Core.hpp"
 #include "AIAC/Client.hpp"
+#include "AIAC/Core.hpp"
 #include "AIAC/Log.hpp"
-
 
 // For more info on imgui structure:https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl3/main.cpp
 
-
-inline static void glfwErrorCallback(int error, const char* description)
-{
+inline static void glfwErrorCallback(int error, const char* description) {
     AIAC_CLI_ERROR("GLFW Error ({0}): {1}", error, description);
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     AIAC::Log::Init();
 
     AIAC_CLI_INFO("augmented_carpentry V:{0}.{1}.{2}", AC_VERSION_MAJOR, AC_VERSION_MINOR, AC_VERSION_PATCH);
 
     AIAC_CLI_INFO("Setting up glfw + GL3");
-    const char* glsl_version = "#version 130"; // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";  // GL 3.0 + GLSL 130
     glfwSetErrorCallback(glfwErrorCallback);
-    if (!glfwInit())
-    {
+    if (!glfwInit()) {
         AIAC_CLI_CRITICAL("Failed to initialize glfw");
         exit(EXIT_FAILURE);
     }
@@ -49,20 +42,19 @@ int main(int argc, char* argv[])
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     GLFWwindow* window = glfwCreateWindow(1280, 720, "augmented_carpentry", nullptr, nullptr);
-    if (window == NULL)
-    {
+    if (window == NULL) {
         AIAC_CLI_CRITICAL("Failed to create GLFW window");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-
+    glfwSwapInterval(1);  // Enable vsync
 
     AIAC_CLI_INFO("Starting ImGUI...");
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
 
     ImGui::StyleColorsDark();
 
@@ -74,8 +66,7 @@ int main(int argc, char* argv[])
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     AIAC_CLI_INFO("Starting main loop...");
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
