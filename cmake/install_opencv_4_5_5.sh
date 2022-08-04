e01a73fd5c8daa62214055130adf46fdba78f1be
9and3 <andreasettimi39@gmail.com>
2022-08-04 10:27:29 +0200

    add auto-installer for opencv4.5.5 and opengl (no version)

--- cmake/install_opencv_4_5_5.sh
+++ cmake/install_opencv_4_5_5.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# TODO: fix this!
-# Install a specific 4.5.5 version needed for the project ac
-cd ~
-# wget https://github.com/opencv/opencv/archive/4.5.5.zip
+sudo apt-get update
+
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev \
+    libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+
+# if directory exists, remove it
+if [ -d "opencv_build" ]; then
+    rm -rf opencv_build
+fi
+mkdir ~/opencv_build && cd ~/opencv_build
+
 wget https://github.com/opencv/opencv_contrib/archive/refs/tags/4.5.5.zip
-unzip opencv_contrib-4.5.5.zip
+unzip 4.5.5.zip
+rm 4.5.5.zip
 wget https://github.com/opencv/opencv/archive/4.5.5.zip
 unzip 4.5.5.zip
-cd opencv-4.5.5
-mkdir build
-cd build
+rm 4.5.5.zip
+
+cd ~/opencv_build/opencv-4.5.5
+mkdir -p build && cd build
+
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_PYTHON_EXAMPLES=ON \
-      -D INSTALL_C_EXAMPLES=OFF \
-      -D OPENCV_ENABLE_NONFREE=ON \
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.5/modules \
-      -D PYTHON_EXECUTABLE=/usr/bin/python3 \
-      -D BUILD_EXAMPLES=ON ..
-make -j4
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib-4.5.5/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j$(nproc)
+
 sudo make install
-sudo ldconfig
-python3 -c "import cv2; print(cv2.__version__)"
+
+pkg-config --modversion opencv4
---
+++ cmake/install_opengl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sudo apt-get update
+
+sudo apt-get install cmake pkg-config
+sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev
