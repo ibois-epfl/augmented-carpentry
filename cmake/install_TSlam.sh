5c371819d0236b0219f9c6084c640e6576002f74
9and3 <andreasettimi39@gmail.com>
2022-08-08 19:51:10 +0200

    add fonts ubuntumono

--- cmake/install_TSlam.sh
+++ cmake/install_TSlam.sh
@@ -3,13 +3,13 @@
 sudo apt-get update
 sudo apt-get install qtbase5-dev libqt5opengl5-dev libopenni2-dev
 
-cd deps/TSlam
+cd ./deps/TSlam
 
 if [ -d build ]; then
     rm -rf build/*
 fi
 mkdir build && cd build
 
-cmake ../ -DBUILD_4_API=OFF
+cmake -DBUILD_4_API=OFF
 make -j$(nproc)
 sudo make install
