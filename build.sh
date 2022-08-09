9a07c1555c8420815589fb49b7a599d77571302c
9and3 <andreasettimi39@gmail.com>
2022-08-09 10:04:38 +0200

    modified build file nproc minus 1 to avoid memory outburst

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d build ]; then
-    make -j$(nproc) -C build
+    make -j$($(nproc)-1) -C build
 else
     echo "[ERROR] The build output dir does not exist. Run configure.sh first."
     exit 1
