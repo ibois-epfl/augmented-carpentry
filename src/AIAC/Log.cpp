fce55d0107d0293c620b3b7f83306ff01cf908f1
9and3 <andreasettimi39@gmail.com>
2022-08-09 10:55:53 +0200

    simplified log system

--- src/AIAC/Log.cpp
+++ src/AIAC/Log.cpp
@@ -4,18 +4,14 @@
 
 namespace AIAC
 {
-    std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-    std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+    std::shared_ptr<spdlog::logger> Log::s_Logger;
 
     void Log::Init()
     {
         spdlog::set_pattern("[source %s] [function %!] [line %#] %^[%T] %n: %v%$");
 
-        s_CoreLogger = spdlog::stdout_color_mt("CORE");
-        s_CoreLogger->set_level(spdlog::level::trace);
-
-        s_ClientLogger = spdlog::stdout_color_mt("CLI");
-        s_ClientLogger->set_level(spdlog::level::trace);
+        s_Logger = spdlog::stdout_color_mt("APP");
+        s_Logger->set_level(spdlog::level::trace);
     }
 
     void Log::Shutdown()
--- src/AIAC/Log.h
+++ src/AIAC/Log.h
@@ -14,33 +14,21 @@ namespace AIAC
         static void Init();
         static void Shutdown();
 
-        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-        inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
 
     private:
-        static std::shared_ptr<spdlog::logger> s_CoreLogger;
-        static std::shared_ptr<spdlog::logger> s_ClientLogger;
+        static std::shared_ptr<spdlog::logger> s_Logger;
     };
 }
 
-#if SILENT_LOGGING
-    #define AIAC_CORE_INFO(...)                     SPDLOG_LOGGER_INFO(AIAC::Log::GetCoreLogger(), __VA_ARGS__)
-    #define AIAC_CORE_WARN(...)                     SPDLOG_LOGGER_WARN(AIAC::Log::GetCoreLogger(), __VA_ARGS__)
-    #define AIAC_CORE_ERROR(...)                    SPDLOG_LOGGER_ERROR(AIAC::Log::GetCoreLogger(), __VA_ARGS__)
-    #define AIAC_CORE_CRITICAL(...)                 SPDLOG_LOGGER_CRITICAL(AIAC::Log::GetCoreLogger(), __VA_ARGS__)
-
-    #define AIAC_CLI_INFO(...)                      SPDLOG_LOGGER_INFO(AIAC::Log::GetClientLogger(), __VA_ARGS__)
-    #define AIAC_CLI_WARN(...)                      SPDLOG_LOGGER_WARN(AIAC::Log::GetClientLogger(), __VA_ARGS__)
-    #define AIAC_CLI_ERROR(...)                     SPDLOG_LOGGER_ERROR(AIAC::Log::GetClientLogger(), __VA_ARGS__)
-    #define AIAC_CLI_CRITICAL(...)                  SPDLOG_LOGGER_CRITICAL(AIAC::Log::GetClientLogger(), __VA_ARGS__)
+#ifdef SILENT_LOGGING
+    #define AIAC_INFO(...)                     SPDLOG_LOGGER_INFO(AIAC::Log::GetLogger(), __VA_ARGS__)
+    #define AIAC_WARN(...)                     SPDLOG_LOGGER_WARN(AIAC::Log::GetLogger(), __VA_ARGS__)
+    #define AIAC_ERROR(...)                    SPDLOG_LOGGER_ERROR(AIAC::Log::GetLogger(), __VA_ARGS__)
+    #define AIAC_CRITICAL(...)                 SPDLOG_LOGGER_CRITICAL(AIAC::Log::GetLogger(), __VA_ARGS__)
 #else
-    #define AIAC_CORE_INFO
-    #define AIAC_CORE_WARN
-    #define AIAC_CORE_ERROR
-    #define AIAC_CORE_CRITICAL
-
-    #define AIAC_CLI_INFO
-    #define AIAC_CLI_WARN
-    #define AIAC_CLI_ERROR
-    #define AIAC_CLI_CRITICAL
+    #define AIAC_INFO
+    #define AIAC_WARN
+    #define AIAC_ERROR
+    #define AIAC_CRITICAL
 #endif
