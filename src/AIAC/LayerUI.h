b0bcb8ed6316cdde8932cb850923588b12b5f29d
9and3 <andreasettimi39@gmail.com>
2022-08-10 14:29:05 +0200

    CAP: import imtexture from memory

--- src/AIAC/LayerUI.h
+++ src/AIAC/LayerUI.h
@@ -2,8 +2,24 @@
 
 #include "AIAC/Layer.h"
 
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#include <GLES2/gl2.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 namespace AIAC
 {
+    struct ImTexture
+        {
+            ImTextureID ID;
+            ImVec2 Size;
+        };
+
     class LayerUI : public AIAC::Layer
     {
     public:
@@ -13,5 +29,14 @@ namespace AIAC
         virtual void OnAttach() override;
         virtual void OnUIRender() override;
         // virtual void OnDetach() override;
+
+    private:
+        bool LoadImgFromFile(const char* path, GLuint& glTextureID, int32_t img_mod);
+
+        void CvtGlTextureID2ImTexture(GLuint glTextureID, ImTexture& imTexture);
+        void CvtGlTextureID2ImTexture(GLuint glTextureID, ImTexture& imTexture, ImVec2 size);
+
+
+
     };
 }
