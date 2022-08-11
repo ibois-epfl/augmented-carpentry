1f878732221af51caeeb30741b787e7772fc2438
9and3 <eyanghongbin0315@gmail.com>
2022-08-11 16:48:15 +0200

    ADD: #include<GL/glew.h>

---
+++ src/AIAC/GlHeader.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __GLHEADER_H__
+#define __GLHEADER_H__
+
+#include <GL/glew.h>
+
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#endif
