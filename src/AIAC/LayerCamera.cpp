ea4325e5a2ed99ac0e9f640128d6afc66ae43f68
9and3 <andreasettimi39@gmail.com>
2022-08-11 18:51:35 +0200

    CAP: basic UI system to add paneUI + FEATUREADD for Image.h

--- src/AIAC/LayerCamera.cpp
+++ src/AIAC/LayerCamera.cpp
@@ -12,10 +12,6 @@ namespace AIAC
 {
     void LayerCamera::OnAttach()
     {
-        AIAC_INFO("LayerCamera attached");
-        AIAC::Application& app = AIAC::Application::GetInstance();
-        AIAC::ApplicationSpecification spec = app.GetSpecification();
-
         try
         {
             MainCamera.Open(0);
@@ -28,8 +24,6 @@ namespace AIAC
 
     void LayerCamera::OnFrameStart()
     {
-        AIAC_INFO("LayerCamera frame start");
-        
         m_CurrentFrame = MainCamera.GetNextFrame();
     }
 }
