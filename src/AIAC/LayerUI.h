2b3b1b98d621517a961bd48e0288c9a8b2385c46
9and3 <eyanghongbin0315@gmail.com>
2022-08-11 16:48:15 +0200

    ADD: #include<GL/glew.h>

--- src/AIAC/LayerUI.h
+++ src/AIAC/LayerUI.h
@@ -2,15 +2,7 @@
 
 #include "AIAC/Layer.h"
 
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <GL/freeglut.h>
-#include <GLES2/gl2.h>
-#include <GLFW/glfw3.h>
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include "AIAC/GlHeader.h"
 
 #include <opencv2/opencv.hpp>
 
