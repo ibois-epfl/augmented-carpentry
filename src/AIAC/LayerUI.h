e23f289541e430ac95d690fb84efb6914d60f874
9and3 <andreasettimi39@gmail.com>
2022-08-11 18:51:35 +0200

    CAP: basic UI system to add paneUI + FEATUREADD for Image.h

--- src/AIAC/LayerUI.h
+++ src/AIAC/LayerUI.h
@@ -1,24 +1,53 @@
 #pragma once
 
-#include "AIAC/Layer.h"
+#include "AIAC/Image.h"
+#include "AIAC/Log.h"
 
+#include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
-#include <GL/freeglut.h>
 #include <GLES2/gl2.h>
 #include <GLFW/glfw3.h>
 
+#include <opencv2/opencv.hpp>
+
+#include "AIAC/Layer.h"
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
-#include <opencv2/opencv.hpp>
+#include <memory>
 
 
-namespace AIAC
-{
-    class LayerUI : public AIAC::Layer
+namespace AIAC {
+
+    
+    class PaneUI
     {
+    public:
+        typedef std::function<void()> Func;
+
+        PaneUI( const char* label, bool isCollapsed, Func func)
+            : m_Label(label), m_IsCollapsed(isCollapsed), m_func( std::move(func) )
+            {}
+
+        template< typename... Args > void Show(Args &&... args)
+        {
+            if(ImGui::CollapsingHeader(m_Label, m_IsCollapsed ? ImGuiTreeNodeFlags_DefaultOpen : 0 ))
+            {
+                m_func( std::forward<Args>(args)... );
+            }
+        }
+
+    private:
+        Func m_func;
+        const char* m_Label;
+        bool m_IsCollapsed;
+    };
+
+
+
+    class LayerUI : public AIAC::Layer {
     public:
         LayerUI() = default;
         virtual ~LayerUI() = default;
@@ -29,9 +58,25 @@ namespace AIAC
         virtual void OnFrameEnd() override;
         virtual void OnFrameFall() override;
         virtual void OnDetach() override;
-    
+
+        void ShowIntroUI();
+
+        inline void StackPane(PaneUI pane) { m_PaneUIStack.push_back(std::make_shared<PaneUI>(pane)); }
+        static void SetPaneUIExample();
+        static void SetPaneUICamera();
+        static void SetPaneUISlam();
+
+
+
     private:
         ImVec4 m_WindowBackColor;
+        AIAC::Image m_Logo;
 
+        bool* m_IsOpen = nullptr;
+
+        // make a vector of PaneUI objects
+        std::vector<std::shared_ptr<PaneUI>> m_PaneUIStack;
+
+        // std::vector<std::shared_ptr<PaneUI<std::function<void()>>>> m_PaneUIs;
     };
 }
