a7a944bf4bbb8c8bc90ea2d32036f06fa1623cd0
Petingo <eyanghongbin0315@gmail.com>
2022-08-12 14:13:30 +0200

    Merge with main

--- src/AIAC/LayerSlam.cpp
+++ src/AIAC/LayerSlam.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+
 namespace AIAC
 {
     // TODO: maybe we should define these const somewhere else?
@@ -28,6 +29,18 @@ namespace AIAC
 
         m_IsTracked = Slam.process(currentFrame, m_CamPose);
 
-        // AIAC_INFO(m_IsTracked);  //FIXME: this goes in the UIpane
+        AIAC_INFO(m_IsTracked);
+        cout << "\n" << m_CamPose << endl;;
+    }
+
+    glm::mat4 LayerSlam::GetCamPoseGlm()
+    {
+        glm::mat4 glmmat;
+        if (m_CamPose.cols != 4 ||m_CamPose.rows != 4 ||m_CamPose.type() != CV_32FC1) {
+            throw std::invalid_argument("GetCamPose() error.");
+        }
+        memcpy(glm::value_ptr(glmmat),m_CamPose.data, 16 * sizeof(float));
+        glmmat = glm::transpose(glmmat);
+        return glmmat;
     }
 }
