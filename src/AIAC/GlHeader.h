ae4c6d6ba0e329be5b9598181612d9f804490b3e
Petingo <eyanghongbin0315@gmail.com>
2022-08-26 18:12:23 +0200

    ADD: Text works

--- src/AIAC/GlHeader.h
+++ src/AIAC/GlHeader.h
@@ -9,6 +9,8 @@
 #include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
 
+#include "glm/glm.hpp"
+
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
--- src/AIAC/Render/RenderAPI.h
+++ src/AIAC/Render/RenderAPI.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "TextRenderer.h"
 #include "GlUtils.h"
+#include "AIAC/GOSys/GOPrimitive.h"
 
 namespace AIAC
 {
@@ -8,5 +10,12 @@ namespace AIAC
     void DrawSlamMap(const shared_ptr<tslam::Map> &map, const glm::vec4 &color, float pointSize);
     void DrawCylinder(const glm::vec3 &baseCenter, const glm::vec3 &topCenter, GLfloat radius, glm::vec4 color, glm::vec4 edgeColor, int sectorNum=24);
 
-    void DrawTest(bool t=true);
+    template<typename T>
+    void Draw(T primObj){
+        if(typeid(primObj) == typeid(GO::GOCylinder)){
+
+        }
+    }
+
+    void DrawTest(bool t=true, glm::mat4 projection=glm::mat4(1.0f));
 }
