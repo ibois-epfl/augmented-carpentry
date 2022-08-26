ad03325374d86c81a4db60faae9a55bc1405bcc8
Petingo <eyanghongbin0315@gmail.com>
2022-08-26 11:52:13 +0200

    ADD: Draw Cylinder API done

--- src/AIAC/Render/RenderAPI.h
+++ src/AIAC/Render/RenderAPI.h
@@ -4,6 +4,9 @@
 
 namespace AIAC
 {
+    glm::vec3 getTransformed(glm::mat4 transformMat, float x, float y, float z);
     void DrawSlamMap(const shared_ptr<tslam::Map> &map, const glm::vec4 &color, float pointSize);
+    void DrawCylinder(const glm::vec3 &baseCenter, const glm::vec3 &topCenter, GLfloat radius, glm::vec4 color, glm::vec4 edgeColor, int sectorNum=24);
+
     void DrawTest(bool t=true);
 }
