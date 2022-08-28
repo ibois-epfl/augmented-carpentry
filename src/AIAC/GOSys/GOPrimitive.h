780b1a9e02eef63bc757b3a5da549c0b882bcdd8
9and3 <andreasettimi39@gmail.com>
2022-08-29 01:16:35 +0200

    MILESTONE: go system working

--- src/AIAC/GOSys/GOPrimitive.h
+++ src/AIAC/GOSys/GOPrimitive.h
@@ -6,27 +6,36 @@
 
 namespace AIAC
 {
-    // enum GOFlags
-    // {
-    //     GO_Flag_None = 0,
-    //     GO_Flag_Highlighted,
-    //     GO_Flag_Selected,
-    //     GO_Flag_Visible,
-    //     GO_Flag_Enabled,
-    // }
-
-    enum GOType
+    enum GOFlags
     {
-        GOTypeNone = 0,
-        GOTypePoint,
-        GOTypeLine,
-        GOTypeCircle,
-        GOTypeCylinder,
-        GOTypePolyline,
-        GOTypeTriangle,
-        GOTypeMesh,
-        GOTypeText
+        GO_Flag_None = 0,
+        GO_Flag_Highlighted,
+        GO_Flag_Selected,
+        GO_Flag_Visible,
+        GO_Flag_Enabled,
     };
+
+    enum GOTypeFlags
+    {
+        _GOPrimitive = 0,
+        _GOPoint,
+        _GOLine,
+        _GOCircle,
+        _GOCylinder,
+        _GOPolyline,
+        _GOTriangle,
+        _GOMesh,
+        _GOText,
+    };
+
+
+    struct GOThickness
+    {
+        static constexpr float Default            = 0.5f;
+        static constexpr float Thin               = 0.1f;
+        static constexpr float Thick              = 1.0f;
+    };
+
 
     enum GOCategory
     {
@@ -35,6 +44,8 @@ namespace AIAC
         GOCategoryCut             = BIT(2),
         GOCategoryVolume          = BIT(3)
     };
+
+
 
     class GOPrimitive
     {
@@ -46,19 +57,15 @@ namespace AIAC
         uint32_t GenerateId();
         inline const uint32_t GetId() const { return m_Id; }
 
-        virtual GOType GetGOType() = 0;
-
         inline GOCategory GetCategory() const { return m_Category; }
         inline bool GetVisibility() { return m_IsVisible; }
         inline glm::vec4 GetColor() { return m_Color; }
         inline bool GetState() { return m_State; }
+        inline GOTypeFlags GetType() { return m_Type; }
 
         inline void SetVisibility(bool isVisible) { m_IsVisible = isVisible; }
         inline void SetColor(glm::vec4 color) { m_Color = color; }
         inline void SetState(bool state) { m_State = state; }
-
-    // private:
-    //     static std::vector<uint32_t> s_Ids;
 
     protected:
         uint32_t m_Id;
@@ -66,46 +73,51 @@ namespace AIAC
         bool m_IsVisible;
         glm::vec4 m_Color;
         bool m_State;
+        GOTypeFlags m_Type;
     };
     using GOPrimitivePointer = std::shared_ptr<GOPrimitive>;
 
-    //TODO: add THICKNESS as property
+
     class GOPoint : public GOPrimitive
     {
     public:
         GOPoint(glm::vec3 position);
         virtual ~GOPoint() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypePoint; }
-
         inline glm::vec3 GetPosition() const { return m_Position; }
+        inline void SetPosition(glm::vec3 position) { m_Position = position; }
         inline float X() const { return m_Position.x; }
         inline float Y() const { return m_Position.y; }
         inline float Z() const { return m_Position.z; }
+        inline void SetX(float x) { m_Position.x = x; }
+        inline void SetY(float y) { m_Position.y = y; }
+        inline void SetZ(float z) { m_Position.z = z; }
+
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
     
     private:
         glm::vec3 m_Position;
+        float m_Thickness = GOThickness::Default;
     };
 
-    //TODO: add THICKNESS as property
 
     class GOLine : public GOPrimitive
     {
     public:
-        GOLine(glm::vec3 p1, glm::vec3 p2);
+        GOLine(GOPoint p1, GOPoint p2);
         virtual ~GOLine() = default;
-
-        virtual GOType GetGOType() override { return GOType::GOTypeLine; }
 
         inline GOPoint GetPStart() const { return m_PStart; }
         inline GOPoint GetPEnd() const { return m_PEnd; }
 
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
+
     private:
         GOPoint m_PStart;
         GOPoint m_PEnd;
+        float m_Thickness = GOThickness::Default;
     };
 
-    //TODO: add THICKNESS as property
 
     class GOCircle : public GOPrimitive
     {
@@ -113,11 +125,12 @@ namespace AIAC
         GOCircle(GOPoint center, float radius);
         virtual ~GOCircle() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypeCircle; }
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
 
     private:
         GOPoint m_Center;
         float m_Radius;
+        float m_Thickness = GOThickness::Default;
     };
 
 
@@ -127,15 +140,12 @@ namespace AIAC
         GOCylinder(GOPoint p1, GOPoint p2, float radius);
         virtual ~GOCylinder() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypeCylinder; }
-
     private:
         GOPoint m_PStart;
         GOPoint m_PEnd;
         float m_Radius;
     };
 
-    //TODO: add THICKNESS as property
 
     class GOPolyline : public GOPrimitive
     {
@@ -143,13 +153,13 @@ namespace AIAC
         GOPolyline(std::vector<GOPoint> points);
         virtual ~GOPolyline() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypePolyline; }
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
 
     private:
         std::vector<GOPoint> m_Points;
+        float m_Thickness = GOThickness::Default;
     };
 
-    //TODO: add THICKNESS as property
 
     class GOTriangle : public GOPrimitive
     {
@@ -157,12 +167,13 @@ namespace AIAC
         GOTriangle(GOPoint p1, GOPoint p2, GOPoint p3);
         virtual ~GOTriangle() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypeTriangle; }
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
 
     private:
         GOPoint m_P1;
         GOPoint m_P2;
         GOPoint m_P3;
+        float m_Thickness = GOThickness::Default;
     };
 
 
@@ -172,14 +183,11 @@ namespace AIAC
         GOMesh(std::vector<GOPoint> points, std::vector<uint32_t> indices);
         virtual ~GOMesh() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypeMesh; }
-
     private:
         std::vector<GOPoint> m_Points;
         std::vector<uint32_t> m_Indices;
     };
 
-    //TODO: add THICKNESS as property
 
     class GOText : public GOPrimitive
     {
@@ -187,11 +195,12 @@ namespace AIAC
         GOText(std::string text, GOPoint anchor, double size);
         virtual ~GOText() = default;
 
-        virtual GOType GetGOType() override { return GOType::GOTypeText; }
+        inline void SetThickness(float thickness) { m_Thickness = thickness; }
 
     private:
         std::string m_Text;
         GOPoint m_Anchor;
         double m_Size;
+        float m_Thickness = GOThickness::Default;
     };
 }
--- src/AIAC/LayerModel.cpp
+++ src/AIAC/LayerModel.cpp
@@ -4,51 +4,57 @@
 #include "AIAC/GOSys/GO.h"
 #include "AIAC/Application.h"
 
+
+//TODO: add the 3D model importer
+
 namespace AIAC
 {
     LayerModel::LayerModel() {}
     
     void LayerModel::OnAttach()
     {
-        // TEST
+        // TODO: erase once the render parsing is done
+        // there is no need for shared_ptr anymore, because the GO is registered in the registry at construction
+        // >>>>>>>>>>>>>> TEST for the Render parsing >>>>>>>>>>>>>>
         std::shared_ptr<GOPoint> pt1 = std::make_shared<GOPoint>(GOPoint(glm::vec3(1, 1, 1)));
         std::shared_ptr<GOPoint> pt2 = std::make_shared<GOPoint>(GOPoint(glm::vec3(2, 2, 2)));
         std::shared_ptr<GOPoint> pt3 = std::make_shared<GOPoint>(GOPoint(glm::vec3(3, 3, 3)));
-
-        const uint32_t id = pt1->GetId();
-        cout << "id: " << id << endl;
-        // // GOPoint pt1(glm::vec3(1, 1, 1));
-
-        // // AIAC_GOREG->Register(pt1.GetSharedPtr());
-
-        AIAC_GOREG->Register(pt1);
-        // AIAC_GOREG->Register(pt2);
-        // AIAC_GOREG->Register(pt3);
-
-        // // // print the content of the registry
-        // // cout << AIAC_GOREG->Count() << endl;
-        // auto aa = AIAC_GOREG->GetGO<GOPoint>(id);
-        // auto aa = AIAC_GOREG->GetGO(id);
-
-        // // use std::visit to get the GO from the registry
-
-
-        // // get GOPoint from variant
-        // std::visit([](auto&& arg) {
-        //     cout << "type: " << arg->GetGOType() << endl;
-        // }, aa);
+        std::shared_ptr<GOLine> line1 = std::make_shared<GOLine>(GOLine(*pt1, *pt2));
+        std::shared_ptr<GOLine> line2 = std::make_shared<GOLine>(GOLine(*pt2, *pt3));
+        std::shared_ptr<GOLine> line3 = std::make_shared<GOLine>(GOLine(*pt3, *pt1));
+        std::shared_ptr<GOCircle> circle1 = std::make_shared<GOCircle>(GOCircle(*pt1, 0.5f));
+        std::shared_ptr<GOCircle> circle2 = std::make_shared<GOCircle>(GOCircle(*pt2, 0.5f));
+        std::shared_ptr<GOCircle> circle3 = std::make_shared<GOCircle>(GOCircle(*pt3, 0.5f));
+        std::shared_ptr<GOCylinder> cylinder1 = std::make_shared<GOCylinder>(GOCylinder(*pt1, *pt2, 0.5f));
+        std::shared_ptr<GOCylinder> cylinder2 = std::make_shared<GOCylinder>(GOCylinder(*pt2, *pt3, 0.5f));
+        std::shared_ptr<GOCylinder> cylinder3 = std::make_shared<GOCylinder>(GOCylinder(*pt3, *pt1, 0.5f));
+        std::shared_ptr<GOPolyline> polyline1 = std::make_shared<GOPolyline>(std::vector<GOPoint>{*pt1, *pt2, *pt3});
+        std::shared_ptr<GOPolyline> polyline2 = std::make_shared<GOPolyline>(std::vector<GOPoint>{*pt2, *pt3, *pt1});
+        std::shared_ptr<GOPolyline> polyline3 = std::make_shared<GOPolyline>(std::vector<GOPoint>{*pt3, *pt1, *pt2});
+        std::shared_ptr<GOTriangle> triangle1 = std::make_shared<GOTriangle>(GOTriangle(*pt1, *pt2, *pt3));
+        std::shared_ptr<GOTriangle> triangle2 = std::make_shared<GOTriangle>(GOTriangle(*pt2, *pt3, *pt1));
+        std::shared_ptr<GOTriangle> triangle3 = std::make_shared<GOTriangle>(GOTriangle(*pt3, *pt1, *pt2));
+        std::shared_ptr<GOMesh> mesh1 = std::make_shared<GOMesh>(std::vector<GOPoint>{*pt1, *pt2, *pt3}, std::vector<uint32_t>{0,1,2});
+        std::shared_ptr<GOText> text1 = std::make_shared<GOText>(GOText("Hello World1", *pt1, 0.5f));
+        std::shared_ptr<GOText> text2 = std::make_shared<GOText>(GOText("Hello World2", *pt2, 0.5f));
+        std::shared_ptr<GOText> text3 = std::make_shared<GOText>(GOText("Hello World3", *pt3, 0.5f));
 
 
-        // cout << typeid(aa).name() << endl;
-        // // cout << aa->X() << endl;
+        // // EXAMPLE how to get the GO from the registry in render
+        // std::vector<std::shared_ptr<GOPoint>> points;
+        // std::vector<std::shared_ptr<GOLine>> lines;
+        // std::vector<std::shared_ptr<GOCircle>> circles;
+        // std::vector<std::shared_ptr<GOCylinder>> cylinders;
+        // std::vector<std::shared_ptr<GOPolyline>> polylines;
+        // std::vector<std::shared_ptr<GOTriangle>> triangles;
+        // std::vector<std::shared_ptr<GOText>> texts;
+        // AIAC_GOREG->GetAllGOs(points, lines, circles, cylinders, polylines, triangles, texts);
 
 
-        // cout << AIAC_GOREG->Count() << endl;
 
-
-        // m_ModelLoader = ModelLoader();
     }
     void LayerModel::OnFrameStart()
     {
+
     }
 }
