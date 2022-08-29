17fa4c83338574c2b5a49682c834ea50799149f0
Petingo <eyanghongbin0315@gmail.com>
2022-08-29 09:34:08 +0200

    ADD: TextRenderer ok (w/o position)

--- src/AIAC/Render/RenderAPI.h
+++ src/AIAC/Render/RenderAPI.h
@@ -6,16 +6,16 @@
 
 namespace AIAC
 {
-    glm::vec3 getTransformed(glm::mat4 transformMat, float x, float y, float z);
+    struct CylinderPole {
+        GLfloat x, z;
+    };
+
+    // ut
+    glm::vec3 GetTransformed(glm::mat4 transformMat, float x, float y, float z);
+
     void DrawSlamMap(const shared_ptr<tslam::Map> &map, const glm::vec4 &color, float pointSize);
-    void DrawCylinder(const glm::vec3 &baseCenter, const glm::vec3 &topCenter, GLfloat radius, glm::vec4 color, glm::vec4 edgeColor, int sectorNum=24);
+    void DrawCylinder(const glm::vec3 &baseCenter, const glm::vec3 &topCenter,
+                             GLfloat radius, glm::vec4 color, glm::vec4 edgeColor, int sectorNum = 24);
 
-    template<typename T>
-    void Draw(T primObj){
-        if(typeid(primObj) == typeid(GO::GOCylinder)){
-
-        }
-    }
-
-    void DrawTest(bool t=true, glm::mat4 projection=glm::mat4(1.0f));
+    void DrawTest(bool t = true, glm::mat4 projection = glm::mat4(1.0f));
 }
