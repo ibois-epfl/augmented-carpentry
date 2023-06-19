0e58c0a9725b7df6b25ad92ec7e6cfda21e22933
9and3 <andreasettimi39@gmail.com>
2023-06-19 19:41:48 +0200

    WIP: building block GLHeader.h ln 6 / Shade.cpp ln 13

--- src/AIAC/GlHeader.h
+++ src/AIAC/GlHeader.h
@@ -3,7 +3,7 @@
 // TODO: put this in the precompiled aiacpch.h file (?)
 
 // <GL/glew.h> has to be included before <GLFW/glfw3.h>
-#include "glew/include/GL/glew.h"
+#include <GL/glew.h>
 
 #include <GL/glut.h>
 #include <GL/freeglut.h>
