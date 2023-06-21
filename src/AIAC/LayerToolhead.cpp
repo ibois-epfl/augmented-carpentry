d55d0587abe7324dfedcc6ced766e8b45cd38e52
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-06-21 19:30:23 +0200

    WIP: try calling ttool

--- src/AIAC/LayerToolhead.cpp
+++ src/AIAC/LayerToolhead.cpp
@@ -2,10 +2,18 @@
 
 #include "AIAC/LayerToolhead.h"
 
+#include <QApplication>
+#include <QThread>
 
 namespace AIAC
 {
-    LayerToolhead::LayerToolhead() {}
+    LayerToolhead::LayerToolhead() {
+        int argc = 1;
+        char* argv[] = { "augmented_carpentry" };
+        QApplication app(argc, argv);
+
+        std::shared_ptr<ttool::TTool> ttool = std::make_shared<ttool::TTool>("/home/tpp/IBOIS/augmented-carpentry/deps/TTool/assets/config.yml", "/home/tpp/IBOIS/augmented-carpentry/deps/TTool/assets/calibration_orange_B_1280_720_r.yml");
+    }
 
     void LayerToolhead::OnAttach() {}
     void LayerToolhead::OnFrameStart() {}
