ef2a5270b128c375ebf583f1e9eb9bce56cffaf9
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2023-06-27 14:30:18 +0200

    WIP: remove unsed is need context flag from layer

--- src/AIAC/Layer.h
+++ src/AIAC/Layer.h
@@ -26,8 +26,5 @@ namespace AIAC
 
         /// Is called when the layer is detached from the application (~app).
         virtual void OnDetach() {}
-
-        bool NeedsContextReleased() const { return false; }
-
     };
 }
