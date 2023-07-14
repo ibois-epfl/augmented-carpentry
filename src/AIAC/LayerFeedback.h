cf63852237283d5ea1d908006c88dd95654ee560
9and3 <andreasettimi39@gmail.com>
2023-07-14 13:29:56 +0200

    FIX: clean missing header

--- src/AIAC/LayerFeedback.h
+++ src/AIAC/LayerFeedback.h
@@ -3,15 +3,22 @@
 #include "AIAC/Layer.h"
 
 
+
 namespace AIAC
 {
     class LayerFeedback : public AIAC::Layer
     {
-    public:
-        LayerFeedback();
-        virtual ~LayerFeedback() = default;
+        public:
+            LayerFeedback();
+            virtual ~LayerFeedback() = default;
 
-        virtual void OnAttach() override;
-        virtual void OnFrameStart() override;
+            virtual void OnAttach() override;
+            virtual void OnFrameStart() override;
+
+        // private:
+            // translation
+            // rotation
+
+
     };
 }
