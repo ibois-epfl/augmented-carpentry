9e26730e4f5c16caec518fbb864c98ae6f8035e0
Petingo <eyanghongbin0315@gmail.com>
2023-07-28 15:56:49 +0200

    Add: Separate FabFeedback file

---
+++ src/AIAC/Feedback/CutCircularSawFeedback.h
@@ -0,0 +1,14 @@
+//
+// Created by ibois on 7/28/23.
+//
+
+#ifndef AC_CUTCIRCULARSAWFEEDBACK_H
+#define AC_CUTCIRCULARSAWFEEDBACK_H
+
+
+class CutCircularSawFeedback {
+
+};
+
+
+#endif //AC_CUTCIRCULARSAWFEEDBACK_H
---
+++ src/AIAC/Feedback/FabFeedback.h
@@ -0,0 +1,15 @@
+//
+// Created by ibois on 7/28/23.
+//
+
+#ifndef AC_FABFEEDBACK_H
+#define AC_FABFEEDBACK_H
+
+
+class FabFeedback {
+    virtual void Update();
+};
+
+
+
+#endif //AC_FABFEEDBACK_H
