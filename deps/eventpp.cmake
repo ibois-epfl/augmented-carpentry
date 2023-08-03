3416a131aa37d9d3f751b64e353a7f992d187757
Nicolas Richart <networms@gmail.ch>
2023-08-03 14:29:04 +0200

    Using external libraries instead of submodules

--- deps/eventpp.cmake
+++ deps/eventpp.cmake
@@ -1,5 +1,5 @@
 download_external_project(eventpp
-    URL "git@github.com:ibois-epfl/eventpp.git"
+    URL "https://www.github.com/ibois-epfl/eventpp.git"
     BACKEND GIT)
 
 add_subdirectory(deps/eventpp)
---
+++ deps/glm.cmake
@@ -0,0 +1,5 @@
+download_external_project(glm
+    URL "https://github.com/ibois-epfl/glm.git"
+    BACKEND GIT)
+
+add_subdirectory(deps/glm)
