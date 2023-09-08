daa39a3f70f6d6a6a284ced824ee96ed3027c778
Andrea Settimi <>
2023-09-08 16:10:49 +0200

    FIX: cmake after merge

--- deps/TTool.cmake
+++ deps/TTool.cmake
@@ -2,3 +2,9 @@ download_submodule_project(TTool)
 
 set(TTOOL_BUILD_TTOOL_EXE OFF)
 add_subdirectory(deps/TTool)
+
+find_package(Torch)
+
+if (TARGET torch_cpu)
+  message("TORCH_CPU_FOUND")
+endif()
