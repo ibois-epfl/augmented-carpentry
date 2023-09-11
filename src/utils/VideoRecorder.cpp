2bffddc79c3686baaaa5805d0b2b62e3fe3c72b3
sushidelivery <nazgul160796@icloud.com>
2023-09-11 17:39:18 +0200

    ADD: video recorder

---
+++ src/utils/VideoRecorder.cpp
@@ -0,0 +1,7 @@
+//
+// Created by zholmaga on 11.09.23.
+//
+
+#include "VideoRecorder.h"
+
+AIAC::Utils::VideoRecorder
---
+++ src/utils/VideoRecorder.h
@@ -0,0 +1,20 @@
+//
+// Created by zholmaga on 11.09.23.
+//
+
+#ifndef AUGMENTED_CARPENTRY_VIDEO_RECORDER_H
+#define AUGMENTED_CARPENTRY_VIDEO_RECORDER_H
+
+namespace AIAC{
+    namespace Utils{
+        class VideoRecorder{
+
+
+
+        };
+    };
+}
+
+
+
+#endif //AUGMENTED_CARPENTRY_VIDEO_RECORDER_H
