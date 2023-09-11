2be496a65751cae2bec4646041452344ab84af4e
Nicolas Richart <networms@gmail.ch>
2023-09-11 11:50:50 +0200

    Adding a first version of the CI

--- deps/TSlam.cmake
+++ deps/TSlam.cmake
@@ -1,3 +1,6 @@
-download_submodule_project(TSlam)
+if(AC_SYNC_GIT_SUBMODULES)
+  download_submodule_project(TSlam)
+endif()
+
 set(TSLAM_QUIET ON)
 add_subdirectory(deps/TSlam)
--- deps/TTool.cmake
+++ deps/TTool.cmake
@@ -1,4 +1,7 @@
-download_submodule_project(TTool)
+if(AC_SYNC_GIT_SUBMODULES)
+  download_submodule_project(TTool)
+endif()
 
 set(TTOOL_BUILD_TTOOL_EXE OFF)
 add_subdirectory(deps/TTool)
+find_package(Torch)
