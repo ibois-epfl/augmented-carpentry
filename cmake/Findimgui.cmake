980b35167ecdc4976f4051f42d0fab66168b8336
sushidelivery <nazgul160796@icloud.com>
2023-09-15 16:52:57 +0200

    FIX: fixed cmake

--- cmake/Findimgui.cmake
+++ cmake/Findimgui.cmake
@@ -1,5 +1,30 @@
+#[[find_package(PkgConfig)
+
+pkg_check_modules(imgui imgui IMPORTED_TARGET)]]
+
+#[[
+message("IMGUI ${imgui_INCLUDE_DIRS}")
+message("IMGUI LIBS ${imgui_LIBRARIES}")
+message("IMGUI LINK LIBS ${imgui_LINK_LIBRARIES}")
+
+target_include_directories(PkgConfig::imgui INTERFACE ${imgui_INCLUDE_DIRS})
+
+
+add_library(imgui IMPORTED GLOBAL)
+set_target_properties(imgui PROPERTIES
+    IMPORTED_LOCATION "${imgui_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${imgui_INCLUDE_DIRS}")
+]]
+
+#[[
+add_library(imgui::imgui ALIAS imgui)
+]]
+
+
 find_package(PkgConfig)
 
 pkg_check_modules(imgui imgui IMPORTED_TARGET)
 
-add_library(imgui::imgui ALIAS PkgConfig::imgui)
+if (imgui_FOUND)
+    add_library(imgui::imgui ALIAS PkgConfig::imgui)
+endif ()
--- deps/imgui.cmake
+++ deps/imgui.cmake
@@ -1,11 +1,14 @@
 download_external_project(imgui
     URL "https://github.com/ibois-epfl/imgui2.git"
-    TAG "release_1_88"
+    TAG "features/cmake_modif_for_ac_integration"
     BACKEND GIT)
 download_external_project(stb
     URL "https://github.com/ibois-epfl/stb.git"
     BACKEND GIT)
 
-add_subdirectory(stb)
-add_subdirectory(imgui)
+#add_subdirectory(stb)
+add_library(stb INTERFACE)
+target_include_directories(stb INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/deps)
+
+add_subdirectory(deps/imgui)
 #list(APPEND AC_INSTALL_TARGETS imgui::imgui)
