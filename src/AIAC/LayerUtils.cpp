c7c8e25030d65e3d89c9aa31e5c7b9fef2c9b157
sushidelivery <nazgul160796@icloud.com>
2023-09-21 18:00:06 +0200

    WIP: LayerUtils source file

--- src/AIAC/LayerUtils.cpp
+++ src/AIAC/LayerUtils.cpp
@@ -10,36 +10,10 @@
 
 namespace AIAC {
 
-    LayerUtils::LayerUtils(){
-
-    }
-    LayerUtils::~LayerUtils() {
-    }
-
-    void LayerUtils::OnAttach() {
-    }
-
-    void LayerUtils::OnDetach() {
-        // Cleanup if needed
-    }
+    void LayerUtils::OnFrameEnd()
+    {
 
 
-    void LayerUtils::OnImGuiRender() {
-        // UI integration for both functionalities
-
-        ImGui::Begin("Utils", NULL);
-
-        m_VideoRecorder.RenderUI();         // VideoRecorder's ImGui UI
-
-        ImGui::End();
-    }
-
-    void LayerUtils::StartRecording() {
-        m_VideoRecorder.Start();
-    }
-
-    void LayerUtils::StopRecording() {
-        m_VideoRecorder.Stop();
     }
 
 }
