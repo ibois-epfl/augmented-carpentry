c7a8449956135c900b305cc754a761520a5b80a2
sushidelivery <nazgul160796@icloud.com>
2023-09-21 13:46:30 +0200

    REFACTOR-CAP: removed comments

--- cmake/Findimgui.cmake
+++ cmake/Findimgui.cmake
@@ -1,26 +1,3 @@
-#[[find_package(PkgConfig)
-
-pkg_check_modules(imgui imgui IMPORTED_TARGET)]]
-
-#[[
-message("IMGUI ${imgui_INCLUDE_DIRS}")
-message("IMGUI LIBS ${imgui_LIBRARIES}")
-message("IMGUI LINK LIBS ${imgui_LINK_LIBRARIES}")
-
-target_include_directories(PkgConfig::imgui INTERFACE ${imgui_INCLUDE_DIRS})
-
-
-add_library(imgui IMPORTED GLOBAL)
-set_target_properties(imgui PROPERTIES
-    IMPORTED_LOCATION "${imgui_LIBRARIES}"
-    INTERFACE_INCLUDE_DIRECTORIES "${imgui_INCLUDE_DIRS}")
-]]
-
-#[[
-add_library(imgui::imgui ALIAS imgui)
-]]
-
-
 find_package(PkgConfig)
 
 pkg_check_modules(imgui imgui IMPORTED_TARGET)
