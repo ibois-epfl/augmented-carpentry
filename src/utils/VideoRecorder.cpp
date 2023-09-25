80a32aa45cb0063f0c318e6d0329619ff305caf0
sushidelivery <nazgul160796@icloud.com>
2023-09-25 15:28:53 +0200

    WIP: initialize the necessary folder for VideoRecorder

--- src/utils/VideoRecorder.cpp
+++ src/utils/VideoRecorder.cpp
@@ -8,22 +8,44 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <filesystem>
 
 namespace AIAC::Utils {
 
     VideoRecorder::VideoRecorder() {
-        std::cout << "VideoRecorder constructor" << std::endl;
+        // check /image folder if it doesn't exist create it
+        std::string imageFolderPath = "./videorecorder";  // Path to the image folder
+        if (!std::filesystem::exists(imageFolderPath)) {
+            std::filesystem::create_directory(imageFolderPath);
+            AIAC_INFO("Created {0} folder", imageFolderPath);
+        }
+        AIAC_INFO("{0} folder exists!", imageFolderPath);
+
+        // create the /frames folder in image folder if image folder exists
+        std::string framesFolderPath = imageFolderPath + "/frames";  // Path to the frames folder
+        if (std::filesystem::exists(imageFolderPath) && !std::filesystem::exists(framesFolderPath)) {
+            std::filesystem::create_directory(framesFolderPath);
+            AIAC_INFO("Created {0} folder", framesFolderPath);
+        }
+        AIAC_INFO("{0} folder exists!", framesFolderPath);
+
+        // create the /video folder in image folder if image folder exists
+        std::string videoFolderPath = imageFolderPath + "/videos";  // Path to the video folder
+        if (std::filesystem::exists(imageFolderPath) && !std::filesystem::exists(videoFolderPath)) {
+            std::filesystem::create_directory(videoFolderPath);
+            AIAC_INFO("Created {0} folder", videoFolderPath);
+        }
     }
 
     VideoRecorder::~VideoRecorder() {
         std::cout << "VideoRecorder destructor" << std::endl;
+
     }
 
     void VideoRecorder::CaptureFrames() {
         std::cout << "VideoRecorder::CaptureFrames" << std::endl;
         int width = AIAC_APP.GetWindow()->GetDisplayW();
         int height = AIAC_APP.GetWindow()->GetDisplayH();
-        std::cout << "Before Frame buffer: width: " << width << " height: " << height << std::endl;
         // create a vector to hold the data
         std::vector<unsigned char> pixels(width * height * 4);  // 4 for RGBA
 
@@ -43,10 +65,10 @@ namespace AIAC::Utils {
 
         boost::uuids::uuid myUuid = boost::uuids::random_generator()();
         cv::imwrite("images/" + boost::uuids::to_string(myUuid) + ".jpg", image);
-        std::cout << "Frame saved" << std::endl;
     }
 
     void VideoRecorder::MakeVideoFromFrames() {
         std::cout << "VideoRecorder::MakeVideoFromFrames" << std::endl;
     }
+
 }
