85b61a647ba236b2b1e5e1be1bff6aa4dce62d7d
sushidelivery <nazgul160796@icloud.com>
2023-09-28 17:02:48 +0200

    WIP: removed unnecessary includes

--- src/AIAC/LayerUtils.cpp
+++ src/AIAC/LayerUtils.cpp
@@ -3,33 +3,26 @@
 //
 
 #include "LayerUtils.h"
-#include "Application.h"
-#include <GL/glew.h>
-#include "Window.h"
-#include <opencv2/opencv.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
-#include "Render/Viewport.h"
 #include "utils/VideoRecorder.h"
 namespace AIAC {
-
     void LayerUtils::OnFrameEnd() {
-        if(m_Recording){
-            videoRecorder->CaptureFrames();
+        std::cout << "LayerUtils::OnFrameEnd" << std::endl;
+        if(this->m_Recording){
+            this->videoRecorder->CaptureFrames();
         }
-
     }
 
     void LayerUtils::StartRecording(){
-        m_Recording = true;
-        videoRecorder = std::make_unique<AIAC::Utils::VideoRecorder>();
+        std::cout << "LayerUtils::StartRecording" << std::endl;
+        this->m_Recording = true;
+        this->videoRecorder = std::make_unique<AIAC::Utils::VideoRecorder>();
     };
 
     void LayerUtils::StopRecording(){
-        m_Recording = false;
-        videoRecorder->MakeVideoFromFrames();
-        videoRecorder.reset();
+        std::cout << "LayerUtils::StopRecording" << std::endl;
+        this->m_Recording = false;
+        this->videoRecorder->MakeVideoFromFrames();
     };
 
 }
