d69c2699b397c852cdcee3aa25a9314d6bb178c2
sushidelivery <nazgul160796@icloud.com>
2023-09-29 14:17:03 +0200

    ADD: comments

--- src/AIAC/LayerUtils.cpp
+++ src/AIAC/LayerUtils.cpp
@@ -7,21 +7,28 @@
 
 namespace AIAC {
     void LayerUtils::OnFrameEnd() {
+        // check if the recording flag is set to true
         if(this->m_Recording){
+            // capture the frames
             this->videoRecorder->CaptureFrames();
         }
     }
 
     void LayerUtils::StartRecording(){
         AIAC_INFO("LayerUtils::StartRecording");
+        // set the recording flag to true
         this->m_Recording = true;
+        // create the video recorder object
         this->videoRecorder = std::make_unique<AIAC::Utils::VideoRecorder>();
     };
 
     void LayerUtils::StopRecording(){
         AIAC_INFO("LayerUtils::StopRecording");
+        // set the recording flag to false
         this->m_Recording = false;
+        // create the video from the frames
         this->videoRecorder->MakeVideoFromFrames();
+        // delete the frames folder and video recorder object
         this->videoRecorder.reset();
     };
 
