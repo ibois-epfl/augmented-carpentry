251ed5bf6264c429a9a5be6c1dd621eff564f538
sushidelivery <nazgul160796@icloud.com>
2023-09-29 14:04:13 +0200

    ADD: comments

--- src/utils/VideoRecorder.h
+++ src/utils/VideoRecorder.h
@@ -11,20 +11,33 @@
 namespace AIAC::Utils {
 
     class VideoRecorder {
-    public:
+    public:///< Public members
         VideoRecorder();
         ~VideoRecorder();
 
+        /// @brief Capture frames from the window
         void CaptureFrames();
+
+        /// @brief Save frames to the frame folder
+        /// @param height Height of the window
+        /// @param width Width of the window
+        /// @param pixels Vector to hold the data
         void SaveFrames(int height, int width, std::vector<unsigned char> pixels);
+
+        /// @brief Create video from the frames
         void MakeVideoFromFrames();
+        /// @brief Create folders for captured frames and videos
         void CreateFolders();
+        /// @brief Delete the frames folder
         void DeleteFrameFolder();
 
-    private:
-        std::string m_ImageFolderPath = "/videorecorder";  // Path to the image folder
-        std::string m_FramesFolderPath = m_ImageFolderPath + "/frames";  // Path to the frames folder
-        std::string m_VideoFolderPath = m_ImageFolderPath + "/videos";  // Path to the video folder
+    private:///< Private members
+        /// Path to the image folder
+        std::string m_ImageFolderPath = "./videorecorder";
+        /// Path to the frames folder
+        std::string m_FramesFolderPath = m_ImageFolderPath + "/frames";
+        /// Path to the video folder
+        std::string m_VideoFolderPath = m_ImageFolderPath + "/videos";
     };
 
 
