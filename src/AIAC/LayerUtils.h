dde0c5d57c904c4b06289d47207ef7e866b0437a
sushidelivery <nazgul160796@icloud.com>
2023-10-02 16:05:09 +0200

    REFACTOR: updated videorecorder variable name to follow naming conventions

--- src/AIAC/LayerUtils.h
+++ src/AIAC/LayerUtils.h
@@ -1,7 +1,3 @@
-//
-// Created by zholmaga on 11.09.23.
-//
-
 #pragma once
 
 #include "AIAC/Layer.h"
@@ -11,7 +7,7 @@
 namespace AIAC {
 
     class LayerUtils : public AIAC::Layer {
-    public:///< Public members
+    public:
         virtual void OnFrameEnd() override;
         /// @brief Start recording the video of a window
         void StartRecording();
@@ -22,14 +18,13 @@ namespace AIAC {
         /// @brief Get the save folder path for the recorder
         std::string GetSaveFolderPath(){return m_SaveFolderPath;};
 
-    private:///< Private members
+    private:
         /// Flag to check if the video is being recorded
         bool m_Recording;
         /// Default path to save the video
         std::string m_SaveFolderPath = AIAC::Config::Get<std::string>(AIAC::Config::SEC_UTILS, AIAC::Config::RECORDER_DEFAULT_PATH);;
         /// Video recorder object
-        std::unique_ptr<AIAC::Utils::VideoRecorder> videoRecorder;
+        std::unique_ptr<AIAC::Utils::VideoRecorder> m_VideoRecorder;
     };
-
 }
 
