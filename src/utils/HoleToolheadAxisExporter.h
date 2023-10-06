6dd4944c52e17997bf415b8b279a9c43ecb76ad0
sushidelivery <nazgul160796@icloud.com>
2023-10-06 15:35:20 +0200

    ADD: added comments and folder creation

--- src/utils/HoleToolheadAxisExporter.h
+++ src/utils/HoleToolheadAxisExporter.h
@@ -7,13 +7,23 @@ namespace AIAC::Utils {
     class HoleToolheadAxisExporter {
     public:
         HoleToolheadAxisExporter();
-        ~HoleToolheadAxisExporter();
+        ~HoleToolheadAxisExporter() = default;
 
+        /// @brief Export the toolhead coordinates
         void ExportToolheadAxis();
+        /// @brief Export the hole coordinates
         void ExportHoleAxis();
-        void WriteCoordToFile(const std::string& objType, const std::string& pointType, std::shared_ptr<GOPoint> goPoint);
+        /**
+         * @brief Write the coordinates to a .log file
+         *
+         * @param objType Type of the item (toolhead or hole)
+         * @param pointType Type of the point (start or end)
+         * @param goPoint Pointer to the GOPoint object to get the coordinates from
+         */
+        void WriteCoordToFile(const std::string& itemType, const std::string& pointType, std::shared_ptr<GOPoint> goPoint);
 
     private:
-        std::string m_SaveToolCoordDefaultPath = AIAC::Config::Get<std::string>(AIAC::Config::SEC_UTILS, AIAC::Config::SAVE_TOOL_COORD_DEFAULT_PATH);
+        /// Default path to save the hole and toolhead coordinates
+        std::string m_SaveCoordDefaultPath = AIAC::Config::Get<std::string>(AIAC::Config::SEC_UTILS, AIAC::Config::SAVE_COORD_DEFAULT_PATH);
     };
 }
