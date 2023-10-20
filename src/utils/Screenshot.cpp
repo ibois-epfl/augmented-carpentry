4c11e2984c6c37818592c984fa1d73c51b87a6f0
sushidelivery <74295705+sushidelivery@users.noreply.github.com>
2023-10-20 10:38:43 +0200

    Features/screenshot frame (#24)
    * ADD-CAP: visualizing the camera feed in grayscale in AC
    
    * WIP: take a snap of calibrated camera buffer and current window
    
    * ADD: added a screenshot class and added it to utils
    
    * ADD: added a screenshot class and added it to utils
    
    * REFACTOR: moved screenshot parts to its own class
    
    * ADD-CAP: added GetColorCurrentFrame to feed into classifier in Toolhead layer
    
    * REFACTOR: getting the color current frame
    
    * REFACTOR: reading the window on the frame end
    
    * REFACTOR: changing the naming format
    
    * REFACTOR: check the result return
    
    * REFACTOR: changed the name of the window capturing function and added the saving images function
    
    * REFACTOR: added comments for doxygen
    
    * REFACTOR: refactored the taking window screenshot function
    
    * REFACTOR: added comments for doxygen and removed unnecessary comments from source code
    
    * REFACTOR: added setting status function and refactored TakeWindowScreenshot
    
    * ADD: screenshots examples

---
+++ src/utils/Screenshot.cpp
@@ -0,0 +1,64 @@
+#include "Screenshot.h"
+#include "AIAC/Config.h"
+#include "../AIAC/Application.h"
+#include "AIAC/LayerCamera.h"
+#include "AIAC/LayerUtils.h"
+
+namespace AIAC::Utils{
+    Screenshot::Screenshot(const std::string& basePath)
+            : m_BasePath(basePath)
+    {
+        LayerUtils::CreateFolder(this->m_BasePath + this->m_ScreenshotPath);
+    }
+
+    void Screenshot::CaptureWindow(){
+        int width = AIAC_APP.GetWindow()->GetDisplayW();
+        int height = AIAC_APP.GetWindow()->GetDisplayH();
+
+        std::vector<unsigned char> pixels(width * height * 4);
+
+        glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
+        cv::Mat image(height, width, CV_8UC4, pixels.data());
+        cv::flip(image, image, 0);
+        cv::cvtColor(image, image, cv::COLOR_RGBA2BGR);
+
+        this->GenerateImageName(true);
+        this->SaveScreenshot(image);
+    }
+
+    void Screenshot::CaptureBuffer() {
+        cv::Mat currentFrame;
+        AIAC_APP.GetLayer<AIAC::LayerCamera>()->MainCamera.GetColorCurrentFrame().GetCvMat().copyTo(currentFrame);
+
+        this->GenerateImageName(false);
+        this->SaveScreenshot(currentFrame);
+    }
+
+    void Screenshot::GenerateImageName(bool isWindow){
+        auto now = std::chrono::system_clock::now();
+        auto timestamp = std::chrono::system_clock::to_time_t(now);
+
+        std::stringstream imagename;
+        if (isWindow) {
+            imagename << "window-";
+        } else {
+            imagename << "buffer-";
+        }
+        struct std::tm* tm;
+        tm = std::localtime(&timestamp);
+
+        imagename << std::put_time(tm, "%Y-%m-%d-%H-%M-%S") << ".png";
+
+        const std::string imagePath = this->m_BasePath + this->m_ScreenshotPath + "/" + imagename.str();
+        this->m_ImageName = imagePath;
+    }
+
+    void Screenshot::SaveScreenshot(cv::Mat image){
+        if(std::filesystem::exists(this->m_BasePath + this->m_ScreenshotPath)) {
+            cv::imwrite(m_ImageName, image);
+            AIAC_INFO("The image is saved to : {0}", m_ImageName);
+        } else {
+            AIAC_ERROR("Error: {0} does not exist!", this->m_BasePath + this->m_ScreenshotPath);
+        }
+    }
+}
