39f9983e22682eaf5b8b2a2014349c5849b52cf6
Andrea Settimi <andreasettimi39@gmail.com>
2024-04-26 15:55:50 +0200

    WIP: first commit for ghpy pipeline

--- invokes/versionize.py
+++ invokes/versionize.py
@@ -1,21 +1,55 @@
 #! python3 
-"""
-    This invoke is responsible to update and overwrite everywhere in the  code base
-    the correct and updated version number of the package/project.
-
-    This version to be adapted everywhere is taken from the `manifest.yml`
-    in the root directory of the project.
-"""
 
 import os
 import sys
+import argparse
+import re
 
 import typing
 
 
-def main() -> None:
+def main(
+    version: str
+) -> None:
     pass
 
 if __name__ == "__main__":
-    #add arguments and description of the  executable in format --name
+    parser = argparse.ArgumentParser(
+        description="Update the version number everywhere in code base."
+    )
+    parser.add_argument(
+        "--version",
+        type=str,
+        help="The version number to update and overwrite in the code base."
+    )
+    parser.add_argument(
+        "--path-manifest",
+        type=str,
+        help="The path to the manifest file."
+    )
+    args = parser.parse_args()
+
+    parse_errors = []
+
+    is_version_ok = True
+    _version = args.version
+    if not re.match(r"^\d+\.\d+\.\d+$", _version) \
+        or _version.count(".") < 2 \
+        or len(_version) < 5:
+        is_version_ok = False
+    else:
+        parse_errors.append("Version must be in the format: Major.Minor.Patch")
     
+    print("Versionizer checks:")
+    if is_version_ok:
+        print("\t[x] Correct version format")
+    else:
+        print(f"\t[ ] Correct version format")
+    
+    if parse_errors.__len__() != 0:
+        print("[ERRORS]:")
+        for error in parse_errors:
+            print(f"\t-{error}")
+
+
+    main(version=_version)
