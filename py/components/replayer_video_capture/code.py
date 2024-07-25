822d9ea2648310e9e6e1ac794b35a640d2271c99
Petingo <eyanghongbin0315@gmail.com>
2024-07-25 15:33:00 +0200

    Fix: wrongly copy-pasted code

--- py/components/replayer_video_capture/code.py
+++ py/components/replayer_video_capture/code.py
@@ -1,14 +1,14 @@
 """Grasshopper Script Instance"""
 import subprocess
-
 import os
+from glob import glob
+
 import Rhino
 
 ## FOR DEBUG
 # import sys
 # sys.path.append("/Users/petingo/p/augmented-carpentry/py/pypi")
 
-import ACPy
 from ACPy.replayer.capture_img import capture_img
 
 class ACPyReplayerCaptureImg(component):
@@ -20,11 +20,21 @@ class ACPyReplayerCaptureImg(component):
             i_frame_count,
             i_frame_rate,
             i_output_path,
-            i_ffmpeg_path):
+            i_ffmpeg_path,
+            i_clean_imgs):
         
         # create folder if not exists
         if not os.path.exists(i_output_path):
             os.makedirs(i_output_path)
+
+        # if the button of clean_img is pressed, only delete the image and do nothing else:
+        if i_clean_imgs:
+            glob_pattern = os.path.join(i_output_path, "frame_*.png")
+            img_files = glob(glob_pattern)
+            for f in img_files:
+                os.remove(f)
+            
+            return
 
         img_output_path = os.path.join(i_output_path, f"frame_{i_frame_idx:06}.png")
         capture_img(img_output_path)
@@ -38,6 +48,7 @@ class ACPyReplayerCaptureImg(component):
             ffmpeg_command = [i_ffmpeg_path, "-y", "-framerate", str(i_frame_rate), "-pattern_type",  "glob", "-i", glob_pattern, "-c:v", "libx264", "-pix_fmt", "yuv420p", video_output_path]
             
             result = subprocess.run(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
             if result.returncode != 0:
                 Rhino.RhinoApp.WriteLine(f"FFmpeg encounter error while generating video.")
