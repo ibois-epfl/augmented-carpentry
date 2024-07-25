40a06be0c6e4f7a0466dfd9b66a41476770141c5
Petingo <eyanghongbin0315@gmail.com>
2024-07-25 16:37:07 +0200

    Fix: image remover

--- py/components/replayer_video_capture/code.py
+++ py/components/replayer_video_capture/code.py
@@ -20,6 +20,7 @@ class ACPyReplayerCaptureImg(component):
             i_frame_count,
             i_frame_rate,
             i_output_path,
+            i_output_name,
             i_ffmpeg_path,
             i_clean_imgs):
         
@@ -27,28 +28,18 @@ class ACPyReplayerCaptureImg(component):
         if not os.path.exists(i_output_path):
             os.makedirs(i_output_path)
 
-        # if the button of clean_img is pressed, only delete the image and do nothing else:
-        if i_clean_imgs:
-            glob_pattern = os.path.join(i_output_path, "frame_*.png")
-            img_files = glob(glob_pattern)
-            for f in img_files:
-                os.remove(f)
-            
-            return
-
         img_output_path = os.path.join(i_output_path, f"frame_{i_frame_idx:06}.png")
         capture_img(img_output_path)
 
         if i_frame_idx == i_frame_count - 1:
             glob_pattern = os.path.join(i_output_path, "frame_*.png")
-            video_output_path = os.path.join(i_output_path, "output.mp4")
+            video_output_path = os.path.join(i_output_path, f"{i_output_name}.mp4")
 
             if i_ffmpeg_path is None or len(i_ffmpeg_path) == 0:
                 i_ffmpeg_path = "ffmpeg"
             ffmpeg_command = [i_ffmpeg_path, "-y", "-framerate", str(i_frame_rate), "-pattern_type",  "glob", "-i", glob_pattern, "-c:v", "libx264", "-pix_fmt", "yuv420p", video_output_path]
             
             result = subprocess.run(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
 
             if result.returncode != 0:
                 Rhino.RhinoApp.WriteLine(f"FFmpeg encounter error while generating video.")
@@ -57,6 +48,12 @@ class ACPyReplayerCaptureImg(component):
                 Rhino.RhinoApp.WriteLine(result.stderr.decode('utf-8'))
                 return False
             else:
+                if i_clean_imgs:
+                    glob_pattern = os.path.join(i_output_path, "frame_*.png")
+                    img_files = glob(glob_pattern)
+                    for f in img_files:
+                        os.remove(f)
+                
                 return True
 
 # if __name__ == "__main__":
