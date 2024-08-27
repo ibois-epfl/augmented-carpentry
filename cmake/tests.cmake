c5687259f30dce80a13406b12e00e0d6dca14cdd
9and3 <andreasettimi39@gmail.com>
2024-08-27 17:51:23 +0200

    WIP-CAP: basic running tests structure

---
+++ cmake/tests.cmake
@@ -0,0 +1,48 @@
+include(CTest)
+include(external_tools)
+enable_testing()
+add_external_package(googletest)
+
+# ------------------------------------------------------------------------------
+# c++
+# ------------------------------------------------------------------------------
+# add new test suites .cc here
+set(AC_TESTSUITE ac_tests)
+add_executable(${AC_TESTSUITE}
+    tests/allCppTests.cc
+    )
+
+target_include_directories(${AC_TESTSUITE}
+PUBLIC
+    ${OpenCV_INCLUDE_DIRS}
+)
+
+target_link_libraries(${AC_TESTSUITE} 
+    ${OpenCV_LIBS}
+    ${TSlam_LIBS}
+    TTool::ttool
+    TSlam::tslam
+    OpenGL::GL
+    OpenGL::GLU
+    imgui::imgui
+    spdlog::spdlog
+    pugixml::pugixml
+    Freetype::Freetype
+    glfw
+    GLEW::GLEW
+    eventpp::eventpp
+    gtest gtest_main
+    )
+
+add_test(NAME ${AC_TESTSUITE} COMMAND ${AC_TESTSUITE})
+
+
+# ------------------------------------------------------------------------------
+# Run all tests
+# ------------------------------------------------------------------------------
+add_custom_command(
+                TARGET ${AC_TESTSUITE} POST_BUILD
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIGURATION> --verbose
+                COMMENT "Running all tests"
+            )
