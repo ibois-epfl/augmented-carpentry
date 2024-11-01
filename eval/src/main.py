57d97b507f2d7b457f4b9f08fe8cf7142ee2293d
Andrea Settimi <andreasettimi39@gmail.com>
2024-11-01 17:36:30 +0100

    WIP-CAP: working on the analysis, decent results

--- eval/src/main.py
+++ eval/src/main.py
@@ -12,6 +12,66 @@ from datetime import datetime
 __time_stamp__: str = datetime.now().strftime('%Y%m%d%H%M%S')
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def clean_missing_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove rows with missing data in the mean column.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to clean.
+
+    Returns:
+        pd.DataFrame: The DataFrame with rows with missing data removed.
+    """
+    df_clean = pd.DataFrame()
+    for i in range(len(df)):
+        rawmean_data = df['mean'][i]
+        if isinstance(rawmean_data, str):
+            continue
+        df_clean = df_clean.append(df.iloc[i], ignore_index=True)
+    df_clean.reset_index(drop=True, inplace=True)
+
+    return df_clean
+
+def process_joint_face_id(df: pd.DataFrame, joint_face_id_col: str, beam_col: str) -> pd.DataFrame:
+    """
+    Process the joint_face_id column to extract joint label, joint face label, and total number of joint faces per joint per beam.
+    And removes the joint_face id column since it is no longer needed.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the joint_face_id column and the beam column.
+        joint_face_id_col (str): The name of the column containing the joint_face_id.
+        beam_col (str): The name of the column containing the beam index.
+
+    Returns:
+        pd.DataFrame: The DataFrame with new columns for joint label, joint face label, and total number of joint faces per joint per beam.
+    """
+    df[['joint_label', 'joint_face_label']] = df[joint_face_id_col].str.split('--', expand=True)[[1, 2]]
+    face_counts = df.groupby([beam_col, 'joint_label'])['joint_face_label'].transform('nunique')
+    df['joint_face_count'] = face_counts
+    df.drop(columns=[joint_face_id_col], inplace=True)
+
+    return df
+
+def remove_single_joint_faces(df: pd.DataFrame, joint_label_col: str, beam_col: str) -> pd.DataFrame:
+    """
+    Remove all rows where there is only a single joint face per joint. We do this because in our analysis methodology, every joint is registered
+    to the CAD joint model and if there is only one face the results would be perfect anyways because we would registering a plane to a plane.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the joint label and beam columns.
+        joint_label_col (str): The name of the column containing the joint label.
+        beam_col (str): The name of the column containing the beam index.
+
+    Returns:
+        pd.DataFrame: The DataFrame with rows removed where there is only a single joint face per joint.
+    """
+    df = df[df['joint_face_count'] > 1]
+    df.reset_index(drop=True, inplace=True)
+
+    return df
 
 
 def main(
@@ -21,72 +81,47 @@ def main(
     ###################################################################################################
     ## Beams - Joint/JointFaces
     ###################################################################################################
+    # ======= importing data into memory =======
     folder_name_beams: str = "a_beams"
     path_dir_beams: str = os.path.join(_path, folder_name_beams)
 
-    pandas_beams_joints_dataset: pd.DataFrame = pd.DataFrame()
-    pandas_beams_joints_dataset['beam_name'] = pd.Series([], dtype=str)
+    df_joints_dataset: pd.DataFrame = pd.DataFrame()
+    df_joints_dataset['beam_name'] = pd.Series([], dtype=str)
 
-    pandas_beams_jointfaces_dataset: pd.DataFrame = pd.DataFrame()
-    pandas_beams_jointfaces_dataset['beam_name'] = pd.Series([], dtype=str)
+    df_jointfaces_dataset: pd.DataFrame = pd.DataFrame()
+    df_jointfaces_dataset['beam_name'] = pd.Series([], dtype=str)
 
     for p in os.listdir(path_dir_beams):
         csv_path = os.path.join(path_dir_beams, p, '_csv')
-        for f in os.listdir(csv_path):
-
-            if f.endswith('_joint.csv'):
+        for i, f in enumerate(os.listdir(csv_path)):
+            if f.endswith('.csv'):
                 abs_path_csv = os.path.abspath(os.path.join(csv_path, f))
                 pandas_data_beam = pd.read_csv(abs_path_csv)
                 pandas_data_beam['beam_name'] = p
-                # -- filtering --
-                # pandas_data_beam = pandas_data_beam[pandas_data_beam['mean'] != 'None']
+                if f.endswith('_joint.csv'):
+                    df_joints_dataset = pd.concat([df_joints_dataset, pandas_data_beam], ignore_index=True)
+                elif f.endswith('_jointfaces.csv'):
+                    df_jointfaces_dataset = pd.concat([df_jointfaces_dataset, pandas_data_beam], ignore_index=True)
 
+    # ======= joint analysis =======
+    df_joints_dataset = clean_missing_data(df_joints_dataset)
 
+    err_joint_mean = np.mean(np.array(df_joints_dataset['mean'].tolist()))
+    err_joint_std = np.mean(np.array(df_joints_dataset['std_deviation'].tolist()))
+    err_joint_total_nbr = len(df_joints_dataset)
 
-                pandas_beams_joints_dataset = pd.concat([pandas_beams_joints_dataset, pandas_data_beam], ignore_index=True)
-            if f.endswith('_jointfaces.csv'):
-                abs_path_csv = os.path.abspath(os.path.join(csv_path, f))
-                pandas_data_beam = pd.read_csv(abs_path_csv)
-                pandas_data_beam['beam_name'] = p
-                pandas_beams_jointfaces_dataset = pd.concat([pandas_beams_jointfaces_dataset, pandas_data_beam], ignore_index=True)
+    # ======= jointfaces analysis =======
+    df_jointfaces_dataset = process_joint_face_id(df_jointfaces_dataset, 'joint_face id', 'beam_name')
+    df_jointfaces_dataset = remove_single_joint_faces(df_jointfaces_dataset, 'joint_label', 'beam_name')
+    df_jointfaces_dataset = clean_missing_data(df_jointfaces_dataset)
 
-    # print the pandas data full length
-    # print(pandas_beams_joints_dataset)
-    print(pandas_beams_jointfaces_dataset)
+    err_jointface_mean = np.mean(np.array(df_jointfaces_dataset['mean'].tolist()))
+    err_jointface_std = np.mean(np.array(df_jointfaces_dataset['std_deviation'].tolist()))
 
-    # calculate the mean of all distances
-    joint_means: typing.List[float] = []
-    joint_std: typing.List[float] = []
-    joint_total_nbr: int = 0
-    # retrieve the value of the column 'mean' and calculate the mean
-    for i in range(len(pandas_beams_joints_dataset)):
-        rawdata_mean = pandas_beams_joints_dataset['mean'][i]
-        rawdata_std = pandas_beams_joints_dataset['std_deviation'][i]
-        # if the value is a string do not consider it
-        if isinstance(rawdata_mean, str):
-            continue
-        joint_means.append(rawdata_mean)
-        joint_std.append(rawdata_std)
-        joint_total_nbr += 1
-    jointfaces_means: typing.List[float] = []
+    print(f"Total number of joint distances: {err_joint_total_nbr}")
+    print(f"Mean of all joint distances: {err_joint_mean}±{err_joint_std}")
+    print(f"Mean of all jointfaces distances: {err_jointface_mean}±{err_jointface_std}")
 
-
-
-    # retrieve the value of the column 'mean' and calculate the mean
-    for i in range(len(pandas_beams_jointfaces_dataset)):
-        rawdata_mean = pandas_beams_jointfaces_dataset['mean'][i]
-        # if the value is a string do not consider it
-        if isinstance(rawdata_mean, str):
-            continue
-        jointfaces_means.append(rawdata_mean)
-
-    print(f'Total number of joint distances: {joint_total_nbr}')
-    print(f'Mean of all joint distances: {sum(joint_means) / len(joint_means)}±{sum(joint_std) / len(joint_std)}')
-
-    print(f'Mean of all jointfaces distances: {sum(jointfaces_means) / len(jointfaces_means)}')
-
-
-    
 
     ###################################################################################################
     ## Assembly
